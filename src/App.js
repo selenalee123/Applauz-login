aa35dd06b78f151c72c6b58b15535fe831143f87
kim <kim>
2023-02-17 22:50:25 -0500

    complete assessment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
+import React, { useState } from 'react';
+import './login.css';
 import logo from './logo.svg';
-import './App.css';
+import Success from './Success';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'testuser' && password === 'testpassword') {
+      setLoggedIn(true);
+    } else {
+      setErrorMessage('Incorrect username or password.');
+    }
+  };
+
+  if (loggedIn) {
+    return <Success />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <div className="login-box">
+        <img src={logo} alt="Applauz logo" className="logo" />
+        <h2>Log In to My Account</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit">Log In</button>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/Success.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Success() {
+    return (
+        <div className="success-container">
+            <h2>Login successful!</h2>
+            <p>You have successfully logged in.</p>
+        </div>
+    );
+}
+
+export default Success;
